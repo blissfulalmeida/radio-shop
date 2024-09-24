1c6f62dbafa9dc716723f56c93949dd6220836f9
Blissful Almeida <blissful.almeida@gmail.com>
2024-09-24 15:11:24 +0300

    Testing

---
+++ src/bet-backup.js
@@ -0,0 +1,66 @@
+const config = require('config');
+const _ = require('lodash');
+const puppeteer = require('puppeteer-core');
+const { createLogger } = require('./components/logger');
+const { OctoBrowserApi } = require('./services/octo-browser');
+
+const logger = createLogger(module);
+
+(async () => {
+    try {
+        logger.info(`Starting application. Partial config: ${JSON.stringify(_.omit(config))}`);
+
+        const octoBrowserApi = new OctoBrowserApi();
+
+        const isOctoRunning = await octoBrowserApi.checkOctoIsRunning();
+
+        if (!isOctoRunning) {
+            throw new Error('OctoBrowser is not running');
+        }
+
+        const neededProfileId = config.get('octoBrowser.profileId');
+
+        const octoBrowserProfile = await octoBrowserApi.connectIfExistsCreateIfNot(neededProfileId);
+
+        logger.info(`Connected to OctoBrowser with profile: ${JSON.stringify(octoBrowserProfile)}`);
+
+        const browser = await puppeteer.connect({
+            browserWSEndpoint: octoBrowserProfile.ws_endpoint,
+            defaultViewport: null,
+        });
+
+        const pages = await browser.pages();
+        const neededPage = pages[0];
+
+        neededPage.waitForSelector('.myb-BetItemsContainer_Container', { timeout: 30000 });
+
+        logger.info('Bets container appeared');
+
+        /** @type {puppeteer.ElementHandle<HTMLDivElement>[]} */
+        const betElements = await neededPage.$$('div.myb-BetItemsContainer_Container > div.myb-OpenBetItem, div.myb-BetItemsContainer_Container > div.myb-SettledBetItem');
+
+        // eslint-disable-next-line no-restricted-syntax
+        for (const betElement of betElements) {
+            // await betElement.screenshot({ path: 'bet.png' });
+            const box = await betElement.boundingBox();
+
+            await neededPage.screenshot({
+                path: 'bet.png',
+                clip: {
+                    x: box.x,
+                    y: box.y,
+                    width: box.width,
+                    height: box.height,
+                },
+            });
+        }
+    } catch (error) {
+        logger.error(`INITIALIZATION_ERROR:: Failed to start application: ${error.message}`);
+
+        process.exitCode = 1;
+
+        setTimeout(() => {
+            process.exit();
+        }, 500);
+    }
+})();
--- testing/beautify.js
+++ testing/beautify.js
@@ -1,7 +1,15 @@
 const fs = require('fs');
 const path = require('path');
+const cheerio = require('cheerio');
 const { beautifyHTML } = require('../src/components/util');
 
-const plainHtmlText = fs.readFileSync(path.join(__dirname, '..', 'crawled', 'bets-2024-09-23T10:24:36.248Z.html'), 'utf8');
-const beautifiedHTML = beautifyHTML(plainHtmlText);
-fs.writeFileSync(path.join(__dirname, '..', 'crawled', 'bets-2024-09-23T10:24:36.248Z.beautified.html'), beautifiedHTML);
+const plainHtmlText = fs.readFileSync(path.join(__dirname, '..', 'crawled', 'bets-2024-09-24T11:35:42.588Z.html'), 'utf8');
+const $ = cheerio.load(plainHtmlText);
+const bets = $('div.myb-OpenBetItem, div.myb-SettledBetItem');
+
+Array.from(bets).forEach((betElement, index) => {
+    const betHtml = $(betElement).html();
+    const beautifiedBetHtml = beautifyHTML(betHtml);
+
+    fs.writeFileSync(path.join(__dirname, `bet-${index}.beautified.html`), beautifiedBetHtml);
+});
