26b432e658f777cd05a48ed81b83e9f0c7fa0c4a
Blissful Almeida <blissful.almeida@gmail.com>
2024-09-24 15:11:41 +0300

    Move to the needed place

--- testing/bet-backup.js
+++ testing/bet-backup.js
@@ -1,8 +1,8 @@
 const config = require('config');
 const _ = require('lodash');
 const puppeteer = require('puppeteer-core');
-const { createLogger } = require('./components/logger');
-const { OctoBrowserApi } = require('./services/octo-browser');
+const { createLogger } = require('../src/components/logger');
+const { OctoBrowserApi } = require('../src/services/octo-browser');
 
 const logger = createLogger(module);
 
