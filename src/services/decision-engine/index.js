17c37342c47833652482cb9761a666b2b3ef5b51
Blissful Almeida <blissful.almeida@gmail.com>
2024-09-27 11:38:51 +0300

    Changing betEngine logic

--- src/services/decision-engine/index.js
+++ src/services/decision-engine/index.js
@@ -1,5 +1,7 @@
 /// <reference path="../../types.js" />
 
+const _ = require('lodash');
+const moment = require('moment');
 const { createLogger } = require('../../components/logger');
 const { BET_365_STATE } = require('../../constants');
 
@@ -37,18 +39,48 @@ class DecisionEngine {
         logger.info(`Fetched ${bets.length} open bets: ${JSON.stringify(bets)}`);
 
         /** @type {BetData[]} */
-        const currentBets = this.storage.get('openBets') || [];
+        const currentBets = _.cloneDeep(this.storage.get('openBets') || []);
+        const betsMap = currentBets.reduce((acc, bet) => acc.set(bet.key, bet), new Map());
 
         // eslint-disable-next-line no-restricted-syntax
         for (const bet of bets) {
-            const betWasSeen = currentBets.some((currentBet) => currentBet.key === bet.key);
+            const betWasAlreadySeen = betsMap.has(bet.key);
 
-            if (!betWasSeen) {
-                this.telegramNotifier.sendNewBetMessage(bet);
+            if (!betWasAlreadySeen) {
+                bet.metadata = {
+                    firstSeenAt: moment.utc().toISOString(),
+                    lastSeenAt: moment.utc().toISOString(),
+                };
+
+                betsMap.set(bet.key, bet);
+            } else {
+                const existingBet = betsMap.get(bet.key);
+
+                existingBet.metadata = bet.metadata || {};
+                existingBet.metadata.lastSeenAt = moment.utc().toISOString();
+
+                if (!existingBet.metadata.firstSeenAt) {
+                    existingBet.metadata.firstSeenAt = moment.utc().toISOString();
+                }
             }
         }
 
-        this.storage.set('openBets', bets);
+        const updatedBets = Array.from(betsMap.values()).sort((a, b) => {
+            const aTime = moment(a.metadata.lastSeenAt);
+            const bTime = moment(b.metadata.lastSeenAt);
+
+            if (aTime.isBefore(bTime)) {
+                return -1;
+            }
+
+            if (aTime.isAfter(bTime)) {
+                return 1;
+            }
+
+            return 0;
+        });
+
+        this.storage.set('openBets', updatedBets);
     }
 }
 
